114e3efb4bd77ea1633add9eaa947786787cb159
Emma Nielsen <Emmanielsen843@gmail.com>
2022-09-19 11:34:50 +0200

    Gallery - JS
    Made a gallery with cats in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,42 @@
 // Objects. 
 const objkat = {
-    kat1: { picture: '/img/elephant.jpg', description: '' },
-    kat2: { picture: '/img/standard_tiger.jpg', description: '' },
-    kat4: { picture: '/img/_WW236934.jpg', description: '' },
-    kat5: { picture: '/img/ferret-table.jpg', description: '' },
-    kat6: { picture: '/img/_H2A0653-1200px.jpg', description: '' }
+    kat1: { picture: '/assets/images/kat1.jpg', description: 'Susanne, 2 måneder' },
+    kat2: { picture: '/assets/images/kat2.jpg', description: 'Cille, 1 år gammel' },
+    kat3: { picture: '/assets/images/kat3.jpg', description: 'Mia, 1 måned' },
+    kat4: { picture: '/assets/images/kat4.jpg', description: 'Steffen, 4 år gammel' },
+    kat5: { picture: '/assets/images/kat5.jpg', description: 'Simon, 3 år og 4 måneder' },
+    kat6: { picture: '/assets/images/kat6.jpg', description: 'Sussie, 6 år gammel og gravid' }
 };
+
+const myGallery = document.getElementById('gallery');
+
+buildGallery()
+
+function buildGallery() {
+    for (let myIndex in objkat) {
+        let myUrl = objkat[myIndex].picture;
+        let description = objkat[myIndex].description;
+
+        buildCard(myUrl, description)
+    }
+}
+
+function buildCard(myUrl, description, clearParent){ 
+    if(clearParent) {
+        myGallery.innerHTML = ' ';
+    }
+
+    const myCard = document.createElement('figure');
+    const cardImage = document.createElement('img');
+    const cardDescription = document.createElement('p');
+
+    cardImage.src = myUrl;
+    cardDescription.innerText = description;
+
+    myCard.appendChild(cardImage);
+    myCard.appendChild(cardDescription);
+
+    myGallery.appendChild(myCard);
+
+
+}
