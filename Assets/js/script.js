587858a7ffc031d062ce649212325243168900d1
Emma Nielsen <Emmanielsen843@gmail.com>
2022-09-19 10:50:12 +0200

    Gallery - js
    Js, not done at all.

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+// Objects. 
+const objkat = {
+    kat1: { picture: '/img/elephant.jpg', description: '' },
+    kat2: { picture: '/img/standard_tiger.jpg', description: '' },
+    kat4: { picture: '/img/_WW236934.jpg', description: '' },
+    kat5: { picture: '/img/ferret-table.jpg', description: '' },
+    kat6: { picture: '/img/_H2A0653-1200px.jpg', description: '' }
+};
